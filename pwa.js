7d11877dd43c7dbf08addba4719cd48357b378fe
feigebaobei <18515195415@163.com>
2022-08-29 07:34:09 +0800

    feat(pwa): 支持强制更新按钮

--- pwa.js
+++ pwa.js
@@ -1,6 +1,7 @@
 let tryRegister = () => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('sw.js').then(registration => {
+        // navigator.serviceWorker.register('swUpdate.js').then(registration => {
             console.log('注册成功', registration)
         }).catch(error => {
             console.log('注册失败', error)
@@ -24,3 +25,29 @@ let showInAppInstallPromotion = () => {
   deferredPrompt.prompt() // 使用暂存的安装事件
   deferredPrompt = null   // 释放内存
 }
+
+// 添加更新按钮
+// 测试通过
+let addUpdateButton = () => {
+    let body = document.querySelector('body')
+    let board = document.createElement('div')
+    let button = document.createElement('button')
+    button.innerText = '强制更新 service worker'
+    let clickButtonHandler = (event) => {
+        console.log('clickButtonHandler', event)
+        navigator.serviceWorker.ready.then((registration) => {
+            console.log('更新了', registration)
+            registration.update()
+        })
+        
+    }
+    button.addEventListener('click', clickButtonHandler)
+    board.appendChild(button)
+    board.style = {
+        position: 'absolute',
+        right: '15px',
+        bottom: '50px',
+    }
+    body.appendChild(board)
+}
+addUpdateButton()
